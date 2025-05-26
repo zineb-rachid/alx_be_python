9070c2ec352195db61c405f91cad38ceadcc6a9b
zineb <zinebrachid@gmail.com>
2025-05-26 12:55:17 +0100

    added numbers and operations and result is showing

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1=10 
+number2=5
+
+# Addition
+addition = number1 + number2
+
+# Subtraction
+subtraction = number1 - number2
+
+# Multiplication
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
