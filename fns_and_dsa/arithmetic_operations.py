62301b4468b6f98df5f97c6b1a950a0310358931
zineb <zinebrachid@gmail.com>
2025-06-13 20:11:07 +0100

    commit the function again

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 from typing import Literal, Union
 
-def perform_operation  (num1 : float, num2 : float,operation:str):
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
