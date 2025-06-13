a74060910374516ea932073d34d2fb2e889d6227
zineb <zinebrachid@gmail.com>
2025-06-13 20:13:43 +0100

    change the zero logic

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,9 +8,9 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num2 != 0:
-            return num1 / num2
+        if num2 == 0:
+             return "Cannot divide by zero"
         else:
-            return "Cannot divide by zero"
+           return num1 / num2
     else:
         return "Invalid operation"
