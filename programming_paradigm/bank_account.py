bf1885421247fddb33f2e175d76c22108a023f4f
zineb <zinebrachid@gmail.com>
2025-06-14 22:34:02 +0100

    correct the display methode

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,4 +11,4 @@ class BankAccount:
         else :
             return False
     def display_balance(self) :
-         print (f"Current Balance: ${self.account_balance: .2f}")
+         print (f"Current Balance: ${self.account_balance:.2f}")
