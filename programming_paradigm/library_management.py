5777c786af5aa8ac3b351176fd2f51892a819197
zineb <zinebrachid@gmail.com>
2025-06-15 23:13:51 +0100

    library added

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,37 +1,52 @@
-class Book:
-    def __init__(self,title,author,):
-        self.title=title
-        self.author=author
-        self._is_checked_out=False
-    def is_available(self):
-     return not self._is_checked_out 
-    def check_book_out(self):
-        if not self._is_checked_out  : # if not means if false the book isn't cheked
-         self._is_checked_out =True
-    def return_book(self):
-           if self._is_checked_out : # if the book cheked (true) make it not cheked because it got returned
-            self._is_checked_out = False 
+# library_management.py
 
+# Book class represents individual books with availability tracking
+class Book:
+    def __init__(self, title, author):  # ✅ Removed the trailing comma
+        self.title = title  # Public attribute: book's title
+        self.author = author  # Public attribute: book's author
+        self._is_checked_out = False  # Private attribute: True if book is checked out
+
+    def check_out(self):
+        """Marks the book as checked out"""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+
+    def return_book(self):
+        """Marks the book as returned (available)"""
+        if self._is_checked_out:
+            self._is_checked_out = False
+
+    def is_available(self):
+        """Returns True if the book is available (not checked out)"""
+        return not self._is_checked_out
+
+
+# Library class manages a collection of books
 class Library:
-    def __init__(self,):
-     self.__books=[] #private list of books 
-    def add_book(self,book):
-        self.__books.append(book)
-    def check_out_book(self,title):
-        for book in self.__books:
-           if book.title == title and book.is_available() :
-               book.check_book_out()
-               break
-    def return_book(self,title):
-        for book in self.__books:
+    def __init__(self):  # ✅ Removed the comma
+        self._books = []  # Private list storing Book instances
+
+    def add_book(self, book):
+        """Adds a Book object to the library collection"""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Checks out the book with the given title if it's available"""
+        for book in self._books:
             if book.title == title and book.is_available():
+                book.check_out()
+                break
+
+    def return_book(self, title):
+        """Returns the book with the given title if it's checked out"""
+        for book in self._books:
+            if book.title == title and not book.is_available():
                 book.return_book()
                 break
-    def list_available_books(self):
-        for book in self.__books:
-            if book.is_available():
-                print(f"{book.title} by {book.author} ")
-    
 
-    
-                
+    def list_available_books(self):
+        """Prints all books that are currently available"""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
